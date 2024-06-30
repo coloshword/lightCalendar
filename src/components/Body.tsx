fbb25aa88636e9da4dbc083c16182af01122d19d
Acero Liang <acero.liangli@stonybrook.edu>
2024-06-30 16:06:14 -0400

    Calendar component

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,11 +1,12 @@
 /** Body: main body component that everything in the main application **/
 import { Header } from "./Header";
-
+import { MainCalendar } from "./MainCalendar";
 const Body = () => {
     return (
         <div className="body">
             {/*header */}
             <Header/>
+            <MainCalendar/>
         </div>
     )
 }
---
+++ src/components/MainCalendar.tsx
@@ -0,0 +1,13 @@
+import "./componentCSS/MainCalendar.css";
+
+interface MainCalendarProps {
+}
+
+const MainCalendar = () => {
+    return (
+        <div className="main-calendar-cont">
+        </div>
+    )
+}
+
+export { MainCalendar };
